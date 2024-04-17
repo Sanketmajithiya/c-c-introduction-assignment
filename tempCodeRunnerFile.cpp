def522a0a659b5361463979109e10216bef6e763
Sanketmajithiya <143784345+Sanketmajithiya@users.noreply.github.com>
2024-04-17 15:56:36 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+# Q.1. Write a C++ Program to print Message " Welcome to Tops Technologies "
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to Tops Technologies" << std::endl;
+    return 0;
+}
